64b7150cb63f5469e32f76c3707b21ee1b961a0b
Rodrigo Melo <nalajejazz@gmail.com>
2022-07-28 01:24:26 -0300

    Add button delete data

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -7,10 +7,11 @@ router.get("/admin/categories/new",(req,res)=>{
                   res.render("admin/categories/new")
 });
 
+///salvar categorias no banco de dados
 router.post("/categories/save",(req,res)=>{
                   var title=req.body.title;
                   if(title !=undefined){
-                                    ///salvar categorias no banco de dados
+                                    
                                     Category.create({
                                                       title:title,
                                                       slug:slugify(title),
@@ -29,6 +30,26 @@ router.get("/admin/categories",(req,res)=>{
                   });
 });
 
+///Deletar categorias do banco de dados
+router.post("/categories/delete",(req,res)=>{
+                  var id= req.body.id;
+                  if(id !=undefined){
+                   if(!isNaN(id)){
+                                    Category.destroy({
+                                                      where:{
+                                                          id:id
+                                                      }
+                                    }).then(()=>{
+                                                      res.redirect("/admin/categories");
+
+                                    });
+                   } else{
+                                    res.redirect("/admin/categories")
+                   }                
+                  }else{
+                                    res.redirect("/admin/categories")          
+                  }
+});               
 
 
 module.exports = router;
